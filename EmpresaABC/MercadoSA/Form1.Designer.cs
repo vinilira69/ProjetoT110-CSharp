7bbd5879169893b968bc569f7fa4f11f825894e7
vinilira69 <vinicius.lirasoliveira@gmail.com>
2023-08-31 17:23:01 -0300

    Sistema

---
+++ EmpresaABC/MercadoSA/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace MercadoSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNome = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.cbbProdutos = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rdbFeminino = new System.Windows.Forms.RadioButton();
+            this.dtpCalendario = new System.Windows.Forms.DateTimePicker();
+            this.ckbLivro = new System.Windows.Forms.CheckBox();
+            this.lstProdutos = new System.Windows.Forms.ListBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(244, 81);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(35, 13);
+            this.lbNome.TabIndex = 0;
+            this.lbNome.Text = "Nome";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(275, 45);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // cbbProdutos
+            // 
+            this.cbbProdutos.FormattingEnabled = true;
+            this.cbbProdutos.Location = new System.Drawing.Point(247, 134);
+            this.cbbProdutos.Name = "cbbProdutos";
+            this.cbbProdutos.Size = new System.Drawing.Size(121, 21);
+            this.cbbProdutos.TabIndex = 2;
+            this.cbbProdutos.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(293, 342);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rdbFeminino
+            // 
+            this.rdbFeminino.AutoSize = true;
+            this.rdbFeminino.Location = new System.Drawing.Point(305, 288);
+            this.rdbFeminino.Name = "rdbFeminino";
+            this.rdbFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rdbFeminino.TabIndex = 4;
+            this.rdbFeminino.TabStop = true;
+            this.rdbFeminino.Text = "Feminino";
+            this.rdbFeminino.UseVisualStyleBackColor = true;
+            // 
+            // dtpCalendario
+            // 
+            this.dtpCalendario.Location = new System.Drawing.Point(477, 191);
+            this.dtpCalendario.Name = "dtpCalendario";
+            this.dtpCalendario.Size = new System.Drawing.Size(200, 20);
+            this.dtpCalendario.TabIndex = 5;
+            // 
+            // ckbLivro
+            // 
+            this.ckbLivro.AutoSize = true;
+            this.ckbLivro.Location = new System.Drawing.Point(457, 50);
+            this.ckbLivro.Name = "ckbLivro";
+            this.ckbLivro.Size = new System.Drawing.Size(49, 17);
+            this.ckbLivro.TabIndex = 6;
+            this.ckbLivro.Text = "Livro";
+            this.ckbLivro.UseVisualStyleBackColor = true;
+            // 
+            // lstProdutos
+            // 
+            this.lstProdutos.FormattingEnabled = true;
+            this.lstProdutos.Location = new System.Drawing.Point(108, 174);
+            this.lstProdutos.Name = "lstProdutos";
+            this.lstProdutos.Size = new System.Drawing.Size(120, 95);
+            this.lstProdutos.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(554, 297);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lstProdutos);
+            this.Controls.Add(this.ckbLivro);
+            this.Controls.Add(this.dtpCalendario);
+            this.Controls.Add(this.rdbFeminino);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbbProdutos);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.lbNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.ComboBox cbbProdutos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton rdbFeminino;
+        private System.Windows.Forms.DateTimePicker dtpCalendario;
+        private System.Windows.Forms.CheckBox ckbLivro;
+        private System.Windows.Forms.ListBox lstProdutos;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
